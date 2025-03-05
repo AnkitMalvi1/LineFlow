0e58619f3a86392b6a757885d939487f14351c49
AnkitMalvi1 <ankitmalviya06219@gmail.com>
2025-03-05 21:52:43 +0530

    remove NODE_ENV from .env and comment out production static file serving in server.js

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ app.use("/api/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/workstations", require("./routes/workstationRoutes"));
 
 // Serve Frontend in Production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
